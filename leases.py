2e64c94d75100978ad7c8166d4c988ef57776394
flame-proof <bounwin@gmail.com>
2016-02-10 16:29:54 +0000

    Update leases.py

--- leases.py
+++ leases.py
@@ -17,7 +17,8 @@ with open('leases.txt', mode='w') as b_file:
 			if a_line[:17] == '  client-hostname':
 				client = a_line[19:-3]
 				if end > now:
-					if os.system('sudo ping -c 1 -w 3 10.227.99.65') == 0:
+					ping_command = 'sudo ping -c 1 -w 1 ' + lease + '> /dev/null'
+					if os.system(ping_command) == 0:
 						response = 'Responding!'
 					else:
 						response = 'No Response'
@@ -27,7 +28,8 @@ with open('leases.txt', mode='w') as b_file:
 			elif a_line[:1] == '}':
 				if written == 0:
 					if end > now:
-						if os.system('sudo ping -c 1 -w 3 10.227.99.65') == 0:
+						ping_command = 'sudo ping -c 1 -w 1 ' + lease + '> /dev/null'
+						if os.system(ping_command) == 0:
 							response = 'Responding!'
 						else:
 							response = 'No Response'
